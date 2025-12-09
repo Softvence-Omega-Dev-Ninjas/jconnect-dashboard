ec43823e0cc32b870b3e1f3465a69e5e776236dc
Md-Ramjan-Ali <gmramjanali8888@gmail.com>
2025-12-09 23:21:15 +0600

    feat: Refactor Payments component to include header and layout for RevenueChart

--- src/pages/Dashboard/Payments/Payments.tsx
+++ src/pages/Dashboard/Payments/Payments.tsx
@@ -1,7 +1,14 @@
+import RevenueChart from "../Dashboard/components/RevenueChart/RevenueChart"
 
 const Payments = () => {
   return (
-    <div>Payments</div>
+     <div className="space-y-4 md:space-y-6">
+      {/* Header */}
+      <div >
+        <h1 className="text-xl md:text-2xl lg:text-[32px] font-bold">Payments</h1>
+      </div>
+      <RevenueChart />
+    </div>
   )
 }
 
