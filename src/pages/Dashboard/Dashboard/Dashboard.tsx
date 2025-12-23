0418cc8cfbecd772c780ecd4562cd01300dc73db
Md-Ramjan-Ali <gmramjanali8888@gmail.com>
2025-12-24 05:04:22 +0600

    fixed all amount by doller

--- src/pages/Dashboard/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard/Dashboard.tsx
@@ -25,7 +25,7 @@ export default function Dashboard() {
         <RevenueChart />
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-5">
         <UserActivity />
         <TopPerformUser />
       </div>
